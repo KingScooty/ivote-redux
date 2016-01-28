dae25227af07e3cc422c734da76f987b38b89442
Scotty Vernon <scott.vernon@gmail.com>
2016-01-28 20:42:20 +0000

    Add more comments

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -16,17 +16,23 @@ export function next(state) {
 /*
 state = {
   vote: {
-    pair: ['Trainspotting', '28 Days Later'],
-    tally: {
-      'Trainspotting': 3,
-      '28 Days Later': 2
-    }
+    pair: ['Trainspotting', '28 Days Later']
   },
   entries: []
 }
 
-updateIn, gotes into vote.tally.trainspotting and initialises it with 0
-if it's unset. It then runs the function to update the value.
+updateIn, gotes into vote.tally.trainspotting, created the keys if they don't already exist, and initialises the final key with 0 if it's unset.
+It then runs the function to update the value.
+
+state = {
+  vote: {
+    pair: ['Trainspotting', '28 Days Later'],
+    tally: {
+      'Trainspotting': 1
+    }
+  },
+  entries: []
+}
 
 Clever!
 
