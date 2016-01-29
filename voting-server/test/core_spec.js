f3c47e994578a03a918a56c03cc9178cbc15767a
Scotty Vernon <scott.vernon@gmail.com>
2016-01-29 17:47:21 +0000

    Core functions and tests.

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -49,6 +49,65 @@ describe('application logic', () => {
       }));
     });
 
+    it('puts winner of current vote back to entries', () => {
+      const state = fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          }
+        },
+        entries: ['Sunshine', 'Millions', '127 Hours']
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: ['Sunshine', 'Millions']
+        },
+        entries: ['127 Hours', 'Trainspotting']
+      }));
+    });
+
+    it('puts both from tied vote back to entries', () => {
+      const state = fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {
+            'Trainspotting': 3,
+            '28 Days Later': 3
+          }
+        },
+        entries: ['Sunshine', 'Millions', '127 Hours']
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(fromJS({
+        vote: {
+          pair: ['Sunshine', 'Millions']
+        },
+        entries: ['127 Hours', 'Trainspotting', '28 Days Later']
+      }));
+    });
+
+    it('marks winner when just one entry is left', () => {
+      const state = fromJS({
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          }
+        },
+        entries: []
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(fromJS({
+        winner: 'Trainspotting'
+      }));
+    });
   });
 
   describe('vote', () => {
@@ -76,18 +135,6 @@ describe('application logic', () => {
     });
 
     it('adds to existing tally for the voted entry', () => {
-      /*
-      const state = Map({
-        vote: Map({
-          pair: List.of('Trainspotting', '28 Days Later'),
-          tally: Map({
-            'Trainspotting': 3,
-            '28 Days Later': 2
-          })
-        }),
-        entries: List()
-      })
-      */
 
       const state = fromJS({
         vote: {
@@ -114,6 +161,7 @@ describe('application logic', () => {
       }));
 
     });
+
   });
 
 });
