b4b8d1e1553b06d80d5703a651500011037ea3fa
Scotty Vernon <scott.vernon@gmail.com>
2016-01-30 08:19:31 +0000

    Load and set entries from json file to init prep state.

--- voting-server/index.js
+++ voting-server/index.js
@@ -1,5 +1,21 @@
 import makeStore from './src/store';
 import startServer from './src/server';
 
+/*
+1. A client sends an action to the server.
+2. The server hands the action to the Redux Store.
+3. The Store calls the reducer and the reducer executes the logic related to the action.
+4. The Store updates its state based on the return value of the reducer.
+5. The Store executes the listener function subscribed by the server.
+6. The server emits a 'state' event.
+7. All connected clients - including the one that initiated the original action - receive the new state.
+*/
+
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
--- voting-server/src/server.js
+++ voting-server/src/server.js
@@ -1,5 +1,34 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // This emits the ENTIRE store. i.e. the ENTIRE
+  // state of the app. For small apps this is okay,
+  // but for larger ones, a subset of the store would be
+  // emitted to the client.
+
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS());
+  );
+
+/*
+  // Equivalent to:
+  store.subscribe(function() {
+    io.emit('state', store.getState().toJS());
+  });
+*/
+
+  // Send state/store to client on connection.
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+
+    // Recieve actions (votes) from client
+    // Security alert: any client can dispatch *any* action.
+    // This would need to be locked down to specific user actions
+    // for production.
+    // http://vertx.io/docs/vertx-web/java/#_securing_the_bridge
+    socket.on('action', store.dispatch.bind(store));
+  })
+
 }
