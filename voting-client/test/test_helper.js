7589c3189b9dcdf550da798407ab3ce4bfe1422d
Scotty Vernon <scott.vernon@gmail.com>
2016-01-30 09:51:28 +0000

    More testing boilerplate.

--- voting-client/test/test_helper.js
+++ voting-client/test/test_helper.js
@@ -1,4 +1,6 @@
 import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const win = doc.defaultView;
@@ -8,8 +10,13 @@ global.window = win;
 
 // Hoist parts of window that for nodejs to use
 // like window.navigator
+
+// This is done so that properties provided by window
+// can be used without the window.
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
     global[key] = window[key];
   }
 });
+
+chai.use(chaiImmutable)
