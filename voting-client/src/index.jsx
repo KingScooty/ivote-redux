69d2427fda69fe89258c322a369a4a7df83c7d50
Scotty Vernon <scott.vernon@gmail.com>
2016-02-02 22:21:55 +0000

    React router and results template.

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route, browserHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting" />,
+  <Router history={browserHistory}>{routes}</Router>,
   document.getElementById('app')
 );
